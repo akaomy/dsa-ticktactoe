725254093364e2e2ffa2a054126594252e595263
akozlova <annakozlova92@gmail.com>
2022-12-10 15:53:01 -0800

    cleam playGame funciton out

--- src/Main.java
+++ src/Main.java
@@ -30,75 +30,13 @@ public class Main {
     
     
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		displayMenu(in);
+		displayMenu();
 
-		
-
-// 
-//        System.out.println(
-//            "X will play first. Enter a slot number to place X in:");
-// 
-//        while (winner == null) {
-//            int numInput;
-//           
-//           // Exception handling.
-//           // numInput will take input from user like from 1 to 9.
-//           // If it is not in range from 1 to 9.
-//           // then it will show you an error "Invalid input."
-//            try {
-//                numInput = in.nextInt();
-//                if (!(numInput > 0 && numInput <= 9)) {
-//                    System.out.println(
-//                        "Invalid input; re-enter slot number:");
-//                    continue;
-//                }
-//            }
-//            catch (InputMismatchException e) {
-//                System.out.println(
-//                    "Invalid input; re-enter slot number:");
-//                continue;
-//            }
-//             
-//            // This game has two player x and O.
-//            // Here is the logic to decide the turn.
-//            if (board[numInput - 1].equals(
-//                    String.valueOf(numInput))) {
-//                board[numInput - 1] = turn;
-// 
-//                if (turn.equals("X")) {
-//                    turn = "O";
-//                }
-//                else {
-//                    turn = "X";
-//                }
-// 
-//                printBoard();
-//                winner = checkWinner();
-//            }
-//            else {
-//                System.out.println(
-//                    "Slot already taken; re-enter slot number:");
-//            }
-//        }
-//       
-//        // If no one win or lose from both player x and O.
-//        // then here is the logic to print "draw".
-//        if (winner.equalsIgnoreCase("draw")) {
-//            System.out.println(
-//                "It's a draw! Thanks for playing.");
-//        }
-//       
-//        // For winner -to display Congratulations! message.
-//        else {
-//            System.out.println(
-//                "Congratulations! " + winner
-//                + "'s have won! Thanks for playing.");
-//        }
-//      in.close();
+ 
     }
 		
-	public static void displayMenu(Scanner in) {
+	public static void displayMenu() {
+		Scanner in = new Scanner(System.in);
 
 		System.out.println("Welcome to 3x3 Tic Tac Toe game.");
 		System.out.println("");
@@ -136,48 +74,37 @@ public class Main {
 		
 		Scanner in = new Scanner(System.in);
 		
+		
 		Queue<String> queue1 = new LinkedList<String>();
 		
 		System.out.println("Please enter first player name:");
 		String player1Name = in.nextLine();
-		
 		System.out.println("Please enter first player age:");
 		int player1Age = in.nextInt();
 
 		Player pl1 = new Player(player1Name, player1Age);
 		
-		queue1.add(player1Name);
+		queue1.add(pl1.getName());
 		
 		
 		Queue<String> queue2 = new LinkedList<String>();
 		
 		System.out.println("Please enter second player name:");
 		String player2Name = in.nextLine();
-		
-		System.out.println("Please enter second player age:");
-		int player2Age = in.nextInt();
 
-		Player pl2 = new Player(player1Name, player1Age);
+		Player pl2 = new Player(player2Name, player1Age);
 		
-		queue1.add(player1Name);
+		queue2.add(pl2.getName());
 		
-		
+	
+		in.close();
 		// go back to menu
-		displayMenu(in);
+//		displayMenu(in);
 	}
 	
 	public static void playGame() {
-      board = new String[9];
-      turn = "X";
-      String winner = null;
-
-      for (int a = 0; a < 9; a++) {
-          board[a] = String.valueOf(a + 1);
-      }
-
-      printBoard();
-      
-    System.out.println("player 1 will play first. Enter a slot number to place X in: ");
+		
+		System.out.println("playGame");
 
 	}
 	
@@ -192,9 +119,7 @@ public class Main {
 	public static void displayTopWinners() {
 		System.out.println("displayTopWinners");
 	}
-	
-	
-	
+		
 	public static void printBoard() {
 		System.out.println("board:");
 		System.out.println("|---|---|---|");
@@ -205,64 +130,62 @@ public class Main {
 		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
 		System.out.println("|---|---|---|");
 	}
-	
-	
+
 	static String checkWinner()
     {
-        for (int a = 0; a < 8; a++) {
-            String line = null;
- 
-            switch (a) {
-            case 0:
-                line = board[0] + board[1] + board[2];
-                break;
-            case 1:
-                line = board[3] + board[4] + board[5];
-                break;
-            case 2:
-                line = board[6] + board[7] + board[8];
-                break;
-            case 3:
-                line = board[0] + board[3] + board[6];
-                break;
-            case 4:
-                line = board[1] + board[4] + board[7];
-                break;
-            case 5:
-                line = board[2] + board[5] + board[8];
-                break;
-            case 6:
-                line = board[0] + board[4] + board[8];
-                break;
-            case 7:
-                line = board[2] + board[4] + board[6];
-                break;
-            }
-            //For X winner
-            if (line.equals("XXX")) {
-                return "X";
-            }
-             
-            // For O winner
-            else if (line.equals("OOO")) {
-                return "O";
-            }
-        }
-         
-        for (int a = 0; a < 9; a++) {
-            if (Arrays.asList(board).contains(
-                    String.valueOf(a + 1))) {
-                break;
-            }
-            else if (a == 8) {
-                return "draw";
-            }
-        }
- 
-       // To enter the X Or O at the exact place on board.
-        System.out.println(
-            turn + "'s turn; enter a slot number to place "
-            + turn + " in:");
-        return null;
+		return "winner";
+//        for (int i = 0; i < 8; i++) {
+//            String line = null;
+// 
+//            switch (i) {
+//            case 0:
+//                line = board[0] + board[1] + board[2];
+//                break;
+//            case 1:
+//                line = board[3] + board[4] + board[5];
+//                break;
+//            case 2:
+//                line = board[6] + board[7] + board[8];
+//                break;
+//            case 3:
+//                line = board[0] + board[3] + board[6];
+//                break;
+//            case 4:
+//                line = board[1] + board[4] + board[7];
+//                break;
+//            case 5:
+//                line = board[2] + board[5] + board[8];
+//                break;
+//            case 6:
+//                line = board[0] + board[4] + board[8];
+//                break;
+//            case 7:
+//                line = board[2] + board[4] + board[6];
+//                break;
+//            }
+//            //For X winner
+//            if (line.equals("XXX")) {
+//                return "X";
+//            }
+//             
+//            // For O winner
+//            else if (line.equals("OOO")) {
+//                return "O";
+//            }
+//        }
+//         
+//        for (int a = 0; a < 9; a++) {
+//            if (Arrays.asList(board).contains(
+//                    String.valueOf(a + 1))) {
+//                break;
+//            }
+//            else if (a == 8) {
+//                return "draw";
+//            }
+//        }
+// 
+//        //To enter the X Or O at the exact place on board.
+//        System.out.println(turn + "'s turn; enter a slot number to place " + turn + " in:");
+//        return null;
     }
 }
